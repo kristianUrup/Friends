f2b5ba31cd5519d3459e8ce8dfa85ef73ec1975f
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-03-09 14:11:54 +0100

    OpenGmail is made

--- app/src/main/java/dk/easv/friendsv2/DetailActivity.java
+++ app/src/main/java/dk/easv/friendsv2/DetailActivity.java
@@ -1,9 +1,12 @@
 package dk.easv.friendsv2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -42,5 +45,14 @@ public class DetailActivity extends AppCompatActivity {
         etName.setText(f.getName());
         etPhone.setText(f.getPhone());
         cbFavorite.setChecked(f.isFavorite());
+    }
+
+    public void openGMail(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Emailing link");
+        intent.putExtra(Intent.EXTRA_TEXT, "Link is \n" +
+                "This is the body of hte message");
+        startActivity(Intent.createChooser(intent, ""));
     }
 }
