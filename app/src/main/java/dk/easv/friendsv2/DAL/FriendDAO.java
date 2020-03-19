c85332cdf5f80d0bd26c3df5e237eb10d43e15c8
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-03-19 11:28:13 +0100

    i have made so we can get 1 friend instead of all

--- app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
+++ app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
@@ -2,16 +2,27 @@ package dk.easv.friendsv2.DAL;
 
 import java.util.List;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import dk.easv.friendsv2.Model.BEFriend;
 
 public class FriendDAO implements IFriendDAO {
+
+
+    private static final String DATABASE_NAME = "sqlite.mDatabase";
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_NAME = "Friend";
+
     @Override
-    public long insert(BEFriend friend) {
+    public long create(BEFriend friend) {
         return 0;
     }
 
     @Override
-    public void deleteById() {
+    public void deleteById(int id) {
 
     }
 
@@ -21,7 +32,12 @@ public class FriendDAO implements IFriendDAO {
     }
 
     @Override
-    public List<BEFriend> selectAll() {
+    public List<BEFriend> getAllFriends() {
+        return null;
+    }
+
+    @Override
+    public BEFriend getFriendById(int id) {
         return null;
     }
 
@@ -29,4 +45,26 @@ public class FriendDAO implements IFriendDAO {
     public void update(BEFriend friendToUpdate) {
 
     }
+
+    private static class OpenHelper extends SQLiteOpenHelper {
+
+        OpenHelper(Context context)
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE " + TABLE_NAME
+                    + "(id INTEGER PRIMARY KEY, name TEXT)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db,
+                              int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
 }
