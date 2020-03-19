bef97531c80a3011dcd34ff4f3cbdbb775cd8d23
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-03-19 11:10:53 +0100

    FriendDAO implementere interface

--- app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
+++ app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
@@ -1,4 +1,32 @@
 package dk.easv.friendsv2.DAL;
 
-public class FriendDAO {
+import java.util.List;
+
+import dk.easv.friendsv2.Model.BEFriend;
+
+public class FriendDAO implements IFriendDAO {
+    @Override
+    public long insert(BEFriend friend) {
+        return 0;
+    }
+
+    @Override
+    public void deleteById() {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<BEFriend> selectAll() {
+        return null;
+    }
+
+    @Override
+    public void update(BEFriend friendToUpdate) {
+
+    }
 }
