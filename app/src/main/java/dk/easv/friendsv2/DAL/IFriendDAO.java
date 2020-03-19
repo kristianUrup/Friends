c58b8d84c229d3a39aca15e189f098845de918c4
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-03-19 11:08:41 +0100

    IFriendDAO is made

---
+++ app/src/main/java/dk/easv/friendsv2/DAL/IFriendDAO.java
@@ -0,0 +1,13 @@
+package dk.easv.friendsv2.DAL;
+
+import java.util.List;
+
+import dk.easv.friendsv2.Model.BEFriend;
+
+public interface IFriendDAO {
+    long insert(BEFriend friend);
+    void deleteById();
+    void deleteAll();
+    List<BEFriend> selectAll();
+    void update(BEFriend friendToUpdate);
+}
