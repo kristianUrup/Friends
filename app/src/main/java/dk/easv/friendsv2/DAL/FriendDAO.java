2284a4ced5dc5be5fc3919ebf11b13446ce881c8
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-03-19 12:04:35 +0100

    Lavet variabler og insert statement

--- app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
+++ app/src/main/java/dk/easv/friendsv2/DAL/FriendDAO.java
@@ -1,10 +1,26 @@
 package dk.easv.friendsv2.DAL;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
 import java.util.List;
 
 import dk.easv.friendsv2.Model.BEFriend;
 
 public class FriendDAO implements IFriendDAO {
+
+    private static final String DATABASE_NAME = "Friend.Database";
+    private static final String TABLE_NAME = "Friends";
+    private static final int VERSION = 2;
+
+    private SQLiteDatabase database;
+    private SQLiteStatement insertStmt;
+
+    public FriendDAO(Context context) {
+        String INSERT = "INSERT INTO " + TABLE_NAME
+                + "(name, phone, isFavorite, image) VALUES(?,?,?,?)";
+    }
 
     @Override
     public long create(BEFriend friend) {
