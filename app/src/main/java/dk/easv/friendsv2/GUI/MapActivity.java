2df6c09f71cd569dd5f21d91bd160855ae0508ab
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-03-26 15:15:28 +0100

    Tilføjet MapActivity hvor kortet skal være

---
+++ app/src/main/java/dk/easv/friendsv2/GUI/MapActivity.java
@@ -0,0 +1,13 @@
+package dk.easv.friendsv2.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
