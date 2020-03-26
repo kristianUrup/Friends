c9b746136adefc14b07e9604ad0b7ab8f985a5dd
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-03-26 15:17:18 +0100

    Update MapActivity.java

--- app/src/main/java/dk/easv/friendsv2/GUI/MapActivity.java
+++ app/src/main/java/dk/easv/friendsv2/GUI/MapActivity.java
@@ -3,6 +3,8 @@ package dk.easv.friendsv2.GUI;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dk.easv.friendsv2.R;
+
 public class MapActivity extends AppCompatActivity {
 
     @Override
